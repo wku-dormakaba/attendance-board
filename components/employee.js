2d60cbeacfa5bdb68df6f45e2c9e2b1f3b24f5d7
William Ku <kucheech@gmail.com>
2022-05-22 17:41:51 +0800

    add markPresence function

--- components/employee.js
+++ components/employee.js
@@ -2,6 +2,10 @@ import { useEffect, useState } from 'react'
 import ToggleButton from 'react-toggle-button'
 import axios from 'axios'
 
+const markPresence = async (id, presence) => {
+  await axios.post('/api/presence', { id, presence })
+}
+
 const Employee = ({ id, name }) => {
 
   const [presence, setPresence] = useState(false)
@@ -21,6 +25,7 @@ const Employee = ({ id, name }) => {
         value={presence}
         onToggle={(value) => {
           setPresence(!value)
+          markPresence(id, !value)
         }} />
       </td>
     </tr>
