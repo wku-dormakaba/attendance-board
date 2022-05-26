2725071a00e04839b3b0ab1e4945eed86a61ffc0
William Ku <kucheech@gmail.com>
2022-05-26 22:42:46 +0800

    setup links to 3 boards

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,38 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { list } from '../data/list'
 import Board from '../components/board'
+import Link from 'next/link'
 
-function refreshPage() {
-  window.location.reload(false);
-}
 
 export default function Home() {
   return (
-    <Board team={{ name: 'Team ABC', list }} />
+    <div className={styles.container}>
+      <div className={styles.grid}>
+        <div className={styles.card}>
+          <h3 className="title">
+            <Link href="/pd-apac">
+              <a>PD APAC</a>
+            </Link>
+          </h3>
+        </div>
+        <div className={styles.card}>
+          <h3 className="title">
+            <Link href="/digital-apac">
+              <a>Digital APAC</a>
+            </Link>
+          </h3>
+        </div>
+        <div className={styles.card}>
+          <h3 className="title">
+            <Link href="/ead-apac">
+              <a>EAD APAC</a>
+            </Link>
+          </h3>
+        </div>
+      </div>
+      <hr />
+      <Board team={{ name: 'Team ABC', list }} />
 
+    </div>
   )
 }
