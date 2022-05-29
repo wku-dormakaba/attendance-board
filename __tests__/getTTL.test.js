381dbfdaba44c3ce94ff9f66568afbb480a1244e
William Ku <kucheech@gmail.com>
2022-05-29 08:23:49 +0800

    specify default timezone to be 'Asia/Singapore' to override deployment timezone

--- __tests__/getTTL.test.js
+++ __tests__/getTTL.test.js
@@ -7,4 +7,14 @@ describe('getTTL', () => {
     expect(ttl).toBeGreaterThanOrEqual(0)
     expect(ttl).toBeLessThan(24 * 60 * 60)
   })
+
+  test('return correct TTL in default timezone', () => {
+    const ttl = getTTL('2022-05-29 08:08')
+    expect(ttl).toBe(57119)
+  })
+
+  test('return correct TTL for different timezones', () => {
+    expect(getTTL('2022-05-29 08:08', 'Asia/Singapore')).toBe(57119)
+    expect(getTTL('2022-05-29 08:08', 'America/New_York')).toBe(13919)
+  })
 })
--- util/getTTL.js
+++ util/getTTL.js
@@ -1,7 +1,11 @@
 const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
-const getTTL = () => {
-  const now = dayjs()
+const getTTL = (datetime, timezone = 'Asia/Singapore') => {
+  const now = dayjs(datetime).tz(timezone)
   return now.endOf('d').diff(now, 's')
 }
 
